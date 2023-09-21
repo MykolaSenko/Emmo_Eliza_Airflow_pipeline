0a61a347c101133dd37036269c59df75ee9c5769
Mykola Senko <mykola-senko@becode.xyz>
2023-09-21 17:40:08 +0200

    automated update from airflow {date}

--- dags/utils/git_push.sh
+++ dags/utils/git_push.sh
@@ -1,2 +1,14 @@
-cd /home/flyingpig/codes/becode_projects/Emmo_Eliza_Airflow_pipeline/
+#!/bin/bash
+
+# Set the path to the SSH key
+AIRFLOW_SSH_KEY=~/.ssh/id_rsa
+
+# Export the SSH key
+export SSH_AUTH_SOCK=$(mktemp -u /tmp/ssh_auth_sock.XXXXXXXX)
+export SSH_AGENT_PID=$(/usr/bin/ssh-agent -a $SSH_AUTH_SOCK)
+/usr/bin/ssh-add "$AIRFLOW_SSH_KEY"
+
+
+#cd /home/flyingpig/codes/becode_projects/Emmo_Eliza_Airflow_pipeline
+# Push the changes to the remote Git repository
 git push
